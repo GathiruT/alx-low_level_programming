338aef3b53b82be9c42a722953b219cf88646bc1
GathiruT <katenganga22@gmail.com>
2023-05-18 18:10:32 -0700

    Intel assembly syntax

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print the size of data types
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Size of int is: %d bytes\n", (int) sizeof(int_type));
+	printf("Size of char is: %d byte\n", (int) sizeof(int_char));
+	printf("Size of long int is: %d bytes\n", (int) sizeof(long int));
+	printf("Size of long long int is: %d bytes\n", (int) sizeof(long long int));
+	printf("Size of a float is: %d bytes\n", (int) sizeof(float));
+
+	return (0);
+}
