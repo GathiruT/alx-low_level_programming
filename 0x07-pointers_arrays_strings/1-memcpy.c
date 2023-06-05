758bd9c60ef60390d6e0bc85664a0c4b626fbbf0
Catherine Gathiru <katenganga22@gmail.com>
2023-06-05 16:22:49 -0700

    Copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,7 +4,7 @@
  * _memcpy - copies memory area
  * @dest: where memory will be copied to
  * @src: memory area
- * @n: bytes 
+ * @n: bytes
  *
  * Return: *dest
  */
@@ -14,7 +14,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		src[i] = dest[i];
+		dest[i] = src[i];
 	}
 
 	return (dest);
