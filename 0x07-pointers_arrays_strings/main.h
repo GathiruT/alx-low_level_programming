b5f15e12cf37b638d4fce07f6f9bd1f108b89801
Catherine Gathiru <katenganga22@gmail.com>
2023-06-05 16:12:56 -0700

    Fills memory with constant byte

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void);
+char *_memset(char *s, char b, unsigned int n);
+void simple_print_buffer(char *buffer, unsigned int size);
+
+#endif
