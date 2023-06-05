1f1bfb4e8ddd431d4290bd4fcf27012709216cfc
Catherine Gathiru <katenganga22@gmail.com>
2023-06-05 16:19:25 -0700

    Copies memory area

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@
 int main(void);
 char *_memset(char *s, char b, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
