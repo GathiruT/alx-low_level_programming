46096c434b3d87826ad6210e85e9a315a86b10f8
Catherine Gathiru <katenganga22@gmail.com>
2023-06-11 21:01:09 -0700

    I just created my first static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - prints a string
+ *
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
