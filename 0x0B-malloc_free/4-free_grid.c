e1c20327ac89b6ef908754ddc017c662cefcb39c
Catherine Gathiru <katenganga22@gmail.com>
2023-06-13 08:41:13 -0700

    Free my grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - frees up memory space of a grid
+ *
+ * @grid: double pointer to the grid
+ * @height: height of the grid
+ * 
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+
+	free(grid);
+}
