501f6a72baf0bee892870ffbe381d1c765a37db4
Catherine Gathiru <katenganga22@gmail.com>
2023-06-20 18:46:38 -0700

    my puppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - dog's info
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: owner's name
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+dog_t;
+
+
+#endif
