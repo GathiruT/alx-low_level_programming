cb7780aba20e4adc3a1d3d6f71d6bbe2be4a776c
Catherine Gathiru <katenganga22@gmail.com>
2023-07-14 05:16:54 -0700

    My files

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(char c);
+
+
+#endif
